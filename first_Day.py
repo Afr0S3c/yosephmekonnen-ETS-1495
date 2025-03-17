842050a479e86b32769d83dbe421ac23c435eef8
afrosec <afr0s3c@gmail.com>
2025-03-17 22:13:54 +0300

    first task to commit

---
+++ first_Day.py
@@ -0,0 +1,17 @@
+#basic python
+
+#string concatination method
+
+x = "hello"
+y = "friend :)"
+
+
+print(x +" "+ y)
+
+#string formating
+
+num = 1
+strr = "meow"
+formated = int(strr)
+
+print(num +" "+formated)
