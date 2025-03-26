4bdb272d337a40f108e0d5cac7f6da8dd68b84cc
afrosec <afr0s3c@gmail.com>
2025-03-26 22:08:04 +0300

    .upper(),.lower() and .title() format

---
+++ third_day.py
@@ -0,0 +1,28 @@
+#.upper(),.lower(),.title(),.capitalize(),.swapcase()
+
+#based on the recent input method that i showed you, you can use the following methods to manipulate the input data
+
+#1--> unpper() method
+
+name = "AfroSec"
+print(name.upper()) #This will print the name called AfroSec in Uppercase
+
+#2--> lower() method
+
+name = "LONEWOLF"
+print(name.lower()) #This will print the name called LONEWOLF in Lowercase
+
+#3--> title() method
+
+name = "afrosec"
+print(name.title()) #This will print the name called afrosec in the title format which is AfroSec
+
+
+
+
+
+
+
+
+
+
