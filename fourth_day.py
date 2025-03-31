5e33e78a6e9784aeaef104115566b85903b51263
afrosec <afr0s3c@gmail.com>
2025-03-31 20:25:52 +0300

    index,startswith,find

---
+++ fourth_day.py
@@ -0,0 +1,21 @@
+#swap
+
+text = "hello friend"
+
+print(text.swapcase())
+
+#find
+
+print(text.find("hello"))
+print(text.find("friend"))
+
+#index
+
+print(text[1])
+
+#startwith
+
+print(text.startswith("hello"))
+print(text.startswith("friend"))
+
+
