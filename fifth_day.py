cfb2fab27c8eb93a94cc6f6875eff7c7c81433df
afrosec <afr0s3c@gmail.com>
2025-03-31 20:37:40 +0300

    count,replace,endswith

---
+++ fifth_day.py
@@ -0,0 +1,11 @@
+# str.endswith()
+text = "hello AfroSec!"
+print(text.endswith("!"))  
+
+# str.count()
+text = "hello hello hello "
+print(text.count("hello"))  
+
+# str.replace()
+text = "wsup AfroSec"
+print(text.replace("AfroSec", "yoseph"))
